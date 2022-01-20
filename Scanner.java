4e800e0d6128cbc181c2ca6adef04fcdacf3aeba
Darkwebhacks <pillssdarknet@gmail.com>
2022-01-20 19:23:16 +0530

    This is Scanner.java

---
+++ Scanner.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Whats your name : ");
+		
+		String name = scan.next();
+		
+	    System.out.print("Whats your age : ");	    
+	    int age = scan.nextInt();
+		
+		
+	}
+}
